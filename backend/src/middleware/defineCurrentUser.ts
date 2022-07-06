d9c5336648fb683bfe0c7e00037ace3c108ef631
Jeremy Parson <jsbparson@gmail.com>
2022-07-06 14:29:51 -0400

    feat(user): added account functionality

---
+++ backend/src/middleware/defineCurrentUser.ts
@@ -0,0 +1,25 @@
+import { User } from "../models";
+const jwt= require("jsonwebtoken")
+import {Request, Response} from "express"
+
+async function defineCurrentUser(req: Request, res: Response, next: Function){
+    try {
+        const [ method, token ] = req.headers.authorization.split(' ')
+        if(method == 'Bearer'){
+            const result = jwt.verify(token, process.env.JWT_SECRET)
+            console.log(method, token, result)
+            const { id } = result
+            let user = await User.findById(id)
+            req.currentUser = user
+            console.log("user defined")
+        }
+        next()
+    } catch(err){
+        console.log("user not defined")
+        console.log(err.message)
+        req.currentUser = null
+        next()
+    }
+}
+
+export default defineCurrentUser;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,36 @@
-import * as express from "express"
+require('dotenv').config()
+import express, {Request, Response} from "express"
+import cors from "cors"
+import {User} from "./models"
+import mongoose from "mongoose"
+
+const app: express.Application = express()
+
+// server middleware
+app.use(
+    express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors())
+
+// middleware
+import defineCurrentUser from "./middleware/defineCurrentUser"
+app.use(defineCurrentUser)
+
+//routes
+import UserRouter from "./routers/user"
+app.use('/user', UserRouter)
+
+import AuthenticationRouter from "./routers/authentication"
+app.use('/auth', AuthenticationRouter)
+
+app.get('/', async (req: Request, res: Response) => {
+  res.json({"message": "conceptiverse api"}).status(200)
+})
+
+// start
+app.listen(process.env.PORT, () => [
+    console.log(`Server listening on port ${process.env.PORT}.`)
+])
